c739dabead43e7667f806e1338de592dc71cce8a
p6825954-coder <p6825964@gmail.com>
2026-06-27 12:56:25 +0700

    Pindahkan kode GhostSpyController ke TestApp

--- app/src/main/java/com/ghostspycontroller/MainActivity.java
+++ app/src/main/java/com/ghostspycontroller/MainActivity.java
@@ -1,12 +1,15 @@
-package com.testapp;
+package com.ghostspycontroller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView status = findViewById(R.id.statusText);
+        status.setText("GhostSpy C2 Ready");
     }
 }
