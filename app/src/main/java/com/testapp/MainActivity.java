6a50ba063affdbe78dcbfb9778b1be233a8e008c
p6825954-coder <p6825964@gmail.com>
2026-06-27 18:43:14 +0700

    Uji koneksi socket ke server GhostSpy

--- app/src/main/java/com/testapp/MainActivity.java
+++ app/src/main/java/com/testapp/MainActivity.java
@@ -2,11 +2,44 @@ package com.testapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import java.net.URISyntaxException;
 
 public class MainActivity extends Activity {
+    private Socket socket;
+    private TextView statusText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        statusText = findViewById(R.id.statusText);
+        statusText.setText("Menghubungkan...");
+
+        try {
+            socket = IO.socket("https://ghostspy.bruang.biz.id");
+        } catch (URISyntaxException e) {
+            statusText.setText("Error: " + e.getMessage());
+            return;
+        }
+
+        socket.on(Socket.EVENT_CONNECT, args -> {
+            runOnUiThread(() -> statusText.setText("Terhubung ke server!"));
+        });
+
+        socket.on(Socket.EVENT_CONNECT_ERROR, args -> {
+            runOnUiThread(() -> statusText.setText("Gagal konek: " + args[0]));
+        });
+
+        socket.connect();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (socket != null) socket.disconnect();
+        super.onDestroy();
     }
 }
