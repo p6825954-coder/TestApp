1f1072ba3f6c70acdd2be88797776b68f2d3ba7a
p6825954-coder <p6825964@gmail.com>
2026-06-27 18:34:47 +0700

    Bersihkan kode lama, pakai Hello World dengan socket.io library

--- app/src/main/java/com/testapp/MainActivity.java
+++ app/src/main/java/com/testapp/MainActivity.java
@@ -1,15 +1,12 @@
-package com.ghostspycontroller;
+package com.testapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView status = findViewById(R.id.statusText);
-        status.setText("GhostSpy C2 Ready");
     }
 }
