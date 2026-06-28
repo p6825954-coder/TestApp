86a56164be25b244259d3f51f639931ee05148ff
p6825954-coder <p6825964@gmail.com>
2026-06-28 17:21:26 +0700

    Login Premium + Dashboard Hello World

--- app/src/main/java/com/testapp/LoginActivity.java
+++ app/src/main/java/com/testapp/LoginActivity.java
@@ -1,12 +1,21 @@
 package com.testapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        });
     }
 }
--- app/src/main/java/com/testapp/MainActivity.java
+++ app/src/main/java/com/testapp/MainActivity.java
@@ -2,17 +2,11 @@ package com.testapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Dashboard Siap");
-        tv.setTextSize(24);
-        tv.setTextColor(0xFFFFFFFF);
-        tv.setBackgroundColor(0xFF0B0E17);
-        setContentView(tv);
+        setContentView(R.layout.activity_main);
     }
 }
